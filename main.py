828342dd782770de30dddd5808a1b51b9c1ade9c
Lobster <72667457+UntitledShovel@users.noreply.github.com>
2023-01-28 09:35:07 -0500

    Some bug fixes and visual changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests, json, random
+import requests, json, random, time, os
+from termcolor import colored
 
 url = "https://random-word-api.herokuapp.com/word?length=5"
 response = requests.get(url)
@@ -8,53 +9,69 @@ url = "https://random-word-api.herokuapp.com/all"
 response = requests.get(url)
 dictionary = json.loads(response.text)
 greyLetters = []
+big_keyboard = "Q W E R T Y U I O P\nA S D F G H J K L\nZ X C V B N M"
+title_lines = ["░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░"
+, "░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗"
+, "░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║"
+, "░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║"
+, "░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝"
+, "░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░"
+, " "
+, "██████╗░██╗░░░██╗██████╗░██╗░░░░░███████╗"
+, "██╔══██╗╚██╗░██╔╝██╔══██╗██║░░░░░██╔════╝"
+, "██████╔╝░╚████╔╝░██║░░██║██║░░░░░█████╗░░"
+, "██╔═══╝░░░╚██╔╝░░██║░░██║██║░░░░░██╔══╝░░"
+, "██║░░░░░░░░██║░░░██████╔╝███████╗███████╗"
+, "╚═╝░░░░░░░░╚═╝░░░╚═════╝░╚══════╝╚══════╝"]
+letters_added_to_keyboard = []
 
-class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+def print_title():
+    global title_lines
+    counter = 0
+
+    for line in title_lines:
+        if counter > 5:
+            print(colored(line, "yellow"))
+        else:
+            print(colored(line, "cyan"))
+        time.sleep(.5)
+        counter += 1
 
 def colorcode(wordGuessed):
-    global word, colors, greyLetters
+    global word, colors, greyLetters, emoji_render
     wordGuessed = [char for char in wordGuessed.lower()]
     orangeLetters = []
 
-
     for i in range(len(wordGuessed)):
         if wordGuessed[i] == word[i]:
-            wordGuessed[i] = colors.OKGREEN + wordGuessed[i] + colors.ENDC
+            wordGuessed[i] = colored(wordGuessed[i], "green")
         elif wordGuessed[i] in word:
             if wordGuessed[i] not in orangeLetters:
                 orangeLetters.append(wordGuessed[i])
-                wordGuessed[i] = colors.FAIL + wordGuessed[i] + colors.ENDC
+                wordGuessed[i] = colored(wordGuessed[i], "yellow")
             else:
                 if wordGuessed[i] not in greyLetters:
                     greyLetters.append(wordGuessed[i])             
-                wordGuessed[i] = colors.OKBLUE + wordGuessed[i] + colors.ENDC
+                    wordGuessed[i] = colored(wordGuessed[i], "dark_grey")
         else:
             if wordGuessed[i] not in greyLetters:
                 greyLetters.append(wordGuessed[i])
-            wordGuessed[i] = colors.OKBLUE + wordGuessed[i] + colors.ENDC
+            wordGuessed[i] = colored(wordGuessed[i], "dark_grey")
     return ''.join(wordGuessed)
 
 
 
 def main():
-    global word, dictionary, greyLetters
+    global word, dictionary, greyLetters, big_keyboard, letters_added_to_keyboard
     guessesLeft = 6
-    print('Welcome to Pydle!')
+    print_title()
     while guessesLeft > 0:
-        x = input(f'Try and guess the word, You have {guessesLeft} guesses left\nYou can also see the letters that are not in the word by typing in "show greys"\n> ')
-        if x == "show greys":
-            print(greyLetters)
-            continue
-        elif not len(x) == 5:
+        if len(greyLetters) > 0:
+            for letter in greyLetters:
+                big_keyboard = big_keyboard.replace(letter.upper(), colored(letter.upper(), "dark_grey"))
+                letters_added_to_keyboard.append(letter)
+        x = input(f'Try and guess the word, You have {guessesLeft} guesses left\n{big_keyboard}\n> ')
+        if not len(x) == 5:
             print('You must guess a 5 letter word')
             continue
         if not x.lower() in dictionary:
@@ -72,3 +89,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    os.system("pause")
